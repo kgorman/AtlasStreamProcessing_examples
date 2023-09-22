2035d6f1023240badfd60a0717ca63ae934dd71c
Kenny Gorman <kenny.gorman@mongodb.com>
2023-09-22 12:46:35 -0500

    new file

---
+++ peeking_at_a_source.js
@@ -0,0 +1,18 @@
+// Running a simple processor to see what is in a stream.
+// In this case it's a change stream source. But this works
+// with Kafka, etc as well.
+
+// a source stage
+source = {$source:
+   {
+      "connectionName":"myAtlasSource", 
+      "db": "test", 
+      "coll": "myTest"
+   }
+}
+
+// create a processor with only 1 stage, just the source
+processor = [s]
+
+// let's take a peek at that stream
+sp.process(s)
